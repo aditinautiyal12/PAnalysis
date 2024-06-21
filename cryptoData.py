3fe9c43aa52cec89c52ca480fb1b3797df16745a
varun prakash <varunprakash652@gmail.com>
2024-06-21 19:34:57 +0530

    Added file creation logic

--- cryptoData.py
+++ cryptoData.py
@@ -1,7 +1,15 @@
 import requests
 import pandas as pd
+from datetime import datetime
 
 response = requests.get("https://api.wazirx.com/sapi/v1/tickers/24hr");
 
 data = response.json()
 df = pd.DataFrame(data)
+
+current_date = datetime.now().strftime("%Y%m%d")
+filename = f"wazirx_data_{current_date}.csv"
+
+df.to_csv(filename, index=False)
+
+print("Data frame stored successfully")
